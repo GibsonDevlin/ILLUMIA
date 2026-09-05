bb72f599a0d32115aa1aaea5b828d56d5ee416a5
Gibson <obodouzogibson@email.com>
2026-09-05 21:48:31 +0100

    creating superuser

---
+++ auth_project/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py createsuperuser --noinput --username admin --email admin@illumia.com 2>/dev/null || true
